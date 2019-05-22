849aa87adf039bf1bbcd89fa60924fce9c278e2c
Yeongbin kang <clear1014@gmail.com>
2019-05-22 23:13:14 +0900

    add examples
    예제 코드를 추가했습니다. 강의 자료에 나오는 ball 예제입니다

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+    public static void main(String[] args){
+        Controller controller = new Controller();
+        controller.init();
+        controller.start();
+    }
+}
